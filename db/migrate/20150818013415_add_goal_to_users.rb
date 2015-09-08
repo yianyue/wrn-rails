057a6e2619c0874bfbd08888a4e7f9525b4e45c0
yianyue <yianyue@gmail.com>
2015-09-08 15:56:38 -0400

    changed migration files for pg

--- db/migrate/20150818013415_add_goal_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddGoalToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :goal, :integer
-  end
-end
--- db/schema_sqlite.rb
+++ db/schema_sqlite.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150824191614) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "entries", force: :cascade do |t|
     t.text     "content"
     t.integer  "word_count"
@@ -27,7 +24,7 @@ ActiveRecord::Schema.define(version: 20150824191614) do
     t.boolean  "locked"
   end
 
-  add_index "entries", ["user_id"], name: "index_entries_on_user_id", using: :btree
+  add_index "entries", ["user_id"], name: "index_entries_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -39,5 +36,4 @@ ActiveRecord::Schema.define(version: 20150824191614) do
     t.string   "token"
   end
 
-  add_foreign_key "entries", "users"
 end
