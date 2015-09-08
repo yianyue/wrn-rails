c81ffb2316b7622a92c1cf0b99bbf47ebb8253d2
yianyue <yianyue@gmail.com>
2015-09-08 16:13:12 -0400

    delete entries first

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.delete_all
 
 Entry.delete_all
+User.delete_all
 
 users = User.create([
   {
